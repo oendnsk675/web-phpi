bfe9cd6ef5d83ecdb9f84e04aec19891442349c0
sayidina ahmadal qososyi <sayidinaahmadalqososyi@gmail.com>
2024-12-30 00:32:16 +0800

    fix path server.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'phpi',
-      script: '/src/server.js',
+      script: './src/server.js',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
     },
   ],
