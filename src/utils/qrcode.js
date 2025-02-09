e59cd78952f64f6bd40ff07cd84d6164a31e2bf6
sayidinaahmadalqososyi <sayidinaahmadalqososyi@gmail.com>
2025-02-09 19:19:14 +0700

    some changes in server

--- src/utils/qrcode.js
+++ src/utils/qrcode.js
@@ -90,7 +90,7 @@ exports.createMemberCard = async (memberInfo) => {
     });
   } catch (error) {
     console.error('Error creating member card:', error);
-    return false;
+    return error;
   }
 };
 
